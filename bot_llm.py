ae476091ff3cfa6181e41a511b982d7091920f34
Kian.Abdalkhani <kabdalkhani@gmail.com>
2025-02-25 21:32:29 -0800

    optimize ollama api call

--- bot_llm.py
+++ bot_llm.py
@@ -1,6 +1,9 @@
 from ollama import Client
 from ollama import ChatResponse
 
+#connect to ollama
+client: Client = Client(host="ollama-services:11434")
+
 #FIXME: need to remove deepseek's '<think>' portions of it's response
 def bot_response(user: str = "user", prompt: str = None) -> str:
   """
@@ -14,9 +17,6 @@ def bot_response(user: str = "user", prompt: str = None) -> str:
     raise ValueError(f"{user} is not a Invalid user, please use 'system' or 'user'")
   if prompt is None:
     raise ValueError("Please enter a prompt.")
-
-  #connect to ollama
-  client: Client = Client(host="ollama-services:11434")
 
   #send prompt to desired LLM model
   response: ChatResponse = client.chat(model="deepseek-r1:1.5b",messages=[
