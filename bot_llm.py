b752bd1738a261a4cde7602777925ad889dadc18
Kian.Abdalkhani <kabdalkhani@gmail.com>
2025-03-04 23:31:48 -0800

    added full test

--- bot_llm.py
+++ bot_llm.py
@@ -1,15 +1,13 @@
 from ollama import Client
 from ollama import ChatResponse
 
-#connect to ollama
-client: Client = Client(host="ollama-services:11434")
-
 #FIXME: need to remove deepseek's '<think>' portions of it's response
-def bot_response(user: str = "user", prompt: str = "") -> str:
+def bot_response(user: str = "user", prompt: str = "", client: Client = None) -> str:
   """
   Create a chat response from the Ollama bot.
   :param user: "user" or "system", the source of the prompt.
   :param prompt: question/statement made to the LLM.
+  :param client: Ollama bot client host
   :return: LLM's response to the prompt.
   """
 
@@ -18,7 +16,9 @@ def bot_response(user: str = "user", prompt: str = "") -> str:
   if prompt == "":
     raise ValueError("Please enter a prompt.")
 
-  #send prompt to desired LLM model
+  # connect to ollama
+  if client is None:
+    client: Client = Client(host="http://localhost:11434")
   response: ChatResponse = client.chat(model="artifish/llama3.2-uncensored:latest",messages=[
     {
       'role': user,
--- tests/test_bot_llm.py
+++ tests/test_bot_llm.py
@@ -1,24 +1,20 @@
 import pytest
 from ..bot_llm import bot_response
 
+
 #TODO: Create a mocker for the ollama API
-class TestBotLLM:
+def test_bot_response_blank():
+    with pytest.raises(ValueError):
+        bot_response(prompt="")
 
-    def test_bot_response_blank(self):
-        with pytest.raises(ValueError):
-            bot_response(prompt="")
+def test_bot_response_invalid_user():
+    with pytest.raises(ValueError):
+        bot_response(user="larry",prompt="Tell me a joke")
 
-    def test_bot_response_empty(self):
-        with pytest.raises(ValueError):
-            bot_response(user="larry",prompt="Tell me a joke")
+def test_bot_response_ollama_api_call(mocker):
+    mock_client = mocker.patch(bot_response.__module__+ ".Client",
+                               autospec=True)
 
-    # def test_returned_values(self,mocker):
-    #     mock_get = mocker.patch("ollama.Client")
-    #     mock_get.return_value.generate.return_value ={
-    #
-    #
-    #     } "To get to the other side"
-    #
-    #     result = bot_response(prompt="Tell me a joke")
-    #     assert result == "To get to the other side"
-    #     mock_get.assert_called_once_with("https://ollama-services:11434/Tell me a joke")
+    bot_response(prompt="Why did the chicken cross the road?")
+    mock_client.assert_called_once_with(host="http://localhost:11434")
+
